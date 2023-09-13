9b53b796305d64b0c0b9d3d7866cbf171ed63f64
Eduardo Hernández Oyarzún <eduardo.he095@gmail.com>
2023-09-13 20:54:46 -0300

    script y readme fix

--- README.md
+++ README.md
@@ -6,15 +6,14 @@ El siguiente proyecto es un portfolio con un simulador de contacto
 
 Para que el proyecto funcione, es necesario ingresar a la sección "**CONTACT**" que se encuentra en el navbar
 
-Se mostrará un mensaje donde debe ingresar una respuesta
+Se inicia el ciclo, donde se mostrará un mensaje donde debe ingresar una respuesta
 
 ```bash
 Posibles respuestas:
  - si
  - no
  - otro (puede ser cualquier otro mensaje)
-```
+ * En el caso de ingresar otro, mostrará un mensaje de alerta para luego volver a mostrar la solicitud inicial
 
-```bash
-En el caso de ingresar otro, mostrará un mensaje de alerta para luego volver a mostrar la solicitud inicial
+ ** Para salir del ciclo, debe escribir no cuando lo solicite
 ```
--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,32 @@
 function message() {
-  let respuestaUsuario = prompt("¿Quieres enviar un mensaje?");
-  if (respuestaUsuario.trim().toLowerCase() == "si") {
-    let nombre = prompt("¿Cual es tu nombre?");
-    let telefono = prompt("¿Cual es tu teléfono?");
-    let email = prompt("¿Cual es tu email?");
-    let motivo = prompt("¿Cual es el motivo de tu mensaje?");
-    let mensaje = prompt("Inserte su mensaje a continuación");
+  while (true) {
+    let respuestaUsuario = prompt("¿Quieres enviar un mensaje? (si/no)");
 
-    alert(
-      "El usuario " +
-        nombre +
-        " con teléfono " +
-        telefono +
-        " y email " +
-        email +
-        " dejó un mensaje que tiene el siguiente motivo: \n\n" +
-        motivo +
-        "\n\ny el mensaje es el siguiente: \n\n\n" +
-        mensaje
-    );
-  } else if (respuestaUsuario.trim().toLowerCase() == "no") {
-    alert("Perfecto, no simularemos un mensaje");
-  } else {
-    alert(
-      "No has ingresado una opción válida, ingresa si o no segun corresponda"
-    );
-    message();
+    if (respuestaUsuario.trim().toLowerCase() === "si") {
+      let nombre = prompt("¿Cuál es tu nombre?");
+      let telefono = prompt("¿Cuál es tu teléfono?");
+      let email = prompt("¿Cuál es tu email?");
+      let motivo = prompt("¿Cuál es el motivo de tu mensaje?");
+      let mensaje = prompt("Inserta tu mensaje a continuación");
+
+      alert(
+        "El usuario " +
+          nombre +
+          " con teléfono " +
+          telefono +
+          " y email " +
+          email +
+          " dejó un mensaje que tiene el siguiente motivo:\n\n" +
+          motivo +
+          "\n\ny el mensaje es el siguiente:\n\n" +
+          mensaje
+      );
+    } else if (respuestaUsuario.trim().toLowerCase() === "no") {
+      alert("Perfecto, no simularemos un mensaje.");
+      break;
+    } else {
+      alert("No has ingresado una opción válida, ingresa 'si' o 'no'.");
+    }
   }
 }
 
